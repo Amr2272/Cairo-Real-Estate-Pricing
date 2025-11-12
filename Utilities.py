7ac7eac130de786d3287b563d88e4a6172a5b7c3
Amr Ashraf <amrforstudy85@gmail.com>
2025-11-12 16:30:51 +0200

    Update Utilities.py

--- Utilities.py
+++ Utilities.py
@@ -11,10 +11,32 @@ def load_data(filepath="cairo_real_estate_dataset.csv"):
         print(f"Error: The file {filepath} was not found.")
         return None
 
+
     df['compound_name'] = df['compound_name'].fillna('Not Specified')
     df['finishing_type'] = df['finishing_type'].fillna('Unfinished')
 
-    columns_to_drop = ['listing_id', 'listing_date', 'days_on_market', 'seller_type', 'is_negotiable']
+    df['listing_date'] = pd.to_datetime(df['listing_date'], errors='coerce')
+
+
+    df['compound_count'] = df.groupby('compound_name')['listing_id'].transform('count')
+
+
+    df['services'] = df['has_security'].map({'Yes':1,"No":0}).fillna(0) + \
+                   df['has_parking'].map({'Yes':1,"No":0}).fillna(0) + \
+                   df['has_balcony'].map({'Yes':1,"No":0}).fillna(0) + \
+                   df['has_amenities'].map({'Yes':1,"No":0}).fillna(0)
+
+    columns_to_drop = [
+        'listing_id',
+        'listing_date',
+        'days_on_market',
+        'seller_type',
+        'is_negotiable',
+        'has_balcony',
+        'has_parking',
+        'has_security',
+        'has_amenities'
+    ]
     df = df.drop(columns=[col for col in columns_to_drop if col in df.columns])
 
     df = df.dropna()
@@ -23,7 +45,6 @@ def load_data(filepath="cairo_real_estate_dataset.csv"):
 
 def get_preprocessor(categorical_features, numeric_features):
 
-    
     numeric_transformer = Pipeline(steps=[
         ('scaler', StandardScaler())
     ])
